936df613bc9d8902203c137eb87d448f85702e6f
EvonuX <djordje42@gmail.com>
2019-02-01 18:05:52 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RandicisionApp from './components/RandicisionApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<RandicisionApp />, document.getElementById('app'));
